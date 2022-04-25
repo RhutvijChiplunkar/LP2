ab895721c830ef403f0afc833bfd2a65bfe3229b
Rhutvij Chiplunkar <rhutvij.chiplunkar@gmail.com>
2022-04-25 21:51:48 +0530

    Wad assignments

--- index.py
+++
@@ -1,9 +0,0 @@
-import webapp2
-
-class MainPage(webapp2.RequestHandler):
-     def get(self):
-	     self.response.write("Bindi")
-
-app = webapp2.WSGIApplication([('/',MainPage),
-			       ],
-                               debug=True)
